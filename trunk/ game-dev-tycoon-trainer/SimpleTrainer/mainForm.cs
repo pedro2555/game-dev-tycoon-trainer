62ef268db500171e87806a360429e33ed8975022
prodrigues1990@gmail.com <prodrigues1990@gmail.com@ea44d2ca-10f4-c6ca-4c27-666fcc95ef56>
2013-06-30 20:57:03 +0000

    First Commit

---
+++ trunk/ game-dev-tycoon-trainer/SimpleTrainer/mainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GameDevTycoon
+{
+    public partial class mainForm : Form
+{
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        public new void Update()
+        {
+            base.Update();
+            if (gdtDBHelper.isGameRunning())
+                return;
+            string blob = gdtDBHelper.getBlob();
+
+            int startIndex = blob.IndexOf("\"company\":{");
+            // Name
+            Regex reg = new Regex("\\\"name\":\"(.*?)\\\","); // "\\ from this (.*?)\\ to this "
+            foreach (Match m in reg.Matches(blob, startIndex))
+            {
+                txtName.Text = m.Groups[1].ToString();
+                startIndex = m.Groups[1].Index;
+                break;
+            }
+            // Cash
+            reg = new Regex("\\\"cash\":(.*?)\\,");
+            foreach (Match m in reg.Matches(blob, startIndex))
+            {
+                txtCash.Text = m.Groups[1].ToString();
+                startIndex = m.Groups[1].Index;
+                break;
+            }
+            // Fans
+            reg = new Regex("\\\"fans\":(.*?)\\,");
+            foreach (Match m in reg.Matches(blob, startIndex))
+            {
+                txtFans.Text = m.Groups[1].ToString();
+                startIndex = m.Groups[1].Index;
+                break;
+            }
+            // Fans
+            reg = new Regex("\\\"researchPoints\":(.*?)\\,");
+            foreach (Match m in reg.Matches(blob, startIndex))
+            {
+                txtResearchPoints.Text = m.Groups[1].ToString();
+                startIndex = m.Groups[1].Index;
+                break;
+            }
+            // Staff
+            // every staff member is in betwen '{"id":' <==> '}'
+            int index = 0;
+            startIndex = blob.IndexOf("\"staff\":[");
+            reg = new Regex("\\{\"id\":(.*?)\\}");
+            foreach (Match m in reg.Matches(blob, startIndex))
+            {
+                // Name
+                Regex reg1 = new Regex("\\,\"name\":\"(.*?)\\\",");
+                foreach (Match m1 in reg1.Matches(m.Groups[1].ToString(), 0))
+                    switch (index)
+                    {
+                        case 0:
+                            txt0Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 1:
+                            txt1Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 2:
+                            txt2Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 3:
+                            txt3Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 4:
+                            txt4Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 5:
+                            txt5Name.Text = m1.Groups[1].ToString();
+                            break;
+                        case 6:
+                            txt6Name.Text = m1.Groups[1].ToString();
+                            break;
+                    }
+                // Design
+                reg1 = new Regex("\\\"dF\":(.*?)\\,");
+
+                index++;
+
+            }
+        }
+
+        private bool SaveBlob()
+        {
+            if (gdtDBHelper.isGameRunning())
+                return false;
+            /// TODO: Test numeric values
+            // Cash
+            try { Decimal.Parse(txtCash.Text); }
+            catch (Exception) { txtCash.BackColor = Color.Red; return false; }
+            txtCash.BackColor = Color.White;
+            // Fans
+            try { Decimal.Parse(txtFans.Text); }
+            catch (Exception) { txtFans.BackColor = Color.Red; return false; }
+            txtFans.BackColor = Color.White;
+            // Research Points
+            try { Decimal.Parse(txtResearchPoints.Text); }
+            catch (Exception) { txtResearchPoints.BackColor = Color.Red; return false; }
+            txtResearchPoints.BackColor = Color.White;
+
+
+
+            string blob = gdtDBHelper.getBlob();
+            string pattern;
+            // Cash
+            pattern = "\\\"cash\":(.*?)\\,"; //    "\\ from this (.*?)\\ to this "
+            foreach (Match m in Regex.Matches(blob, pattern))
+            {
+                blob = blob.Remove(m.Groups[1].Index, m.Groups[1].Length);
+                blob = blob.Insert(m.Groups[1].Index, txtCash.Text);
+                break;
+            }
+            // Fans
+            pattern = "\\\"fans\":(.*?)\\,"; //    "\\ from this (.*?)\\ to this "
+            foreach (Match m in Regex.Matches(blob, pattern))
+            {
+                blob = blob.Remove(m.Groups[1].Index, m.Groups[1].Length);
+                blob = blob.Insert(m.Groups[1].Index, txtFans.Text);
+                break;
+            }
+            // Reserach Points
+            pattern = "\\\"researchPoints\":(.*?)\\,"; //    "\\ from this (.*?)\\ to this "
+            foreach (Match m in Regex.Matches(blob, pattern))
+            {
+                blob = blob.Remove(m.Groups[1].Index, m.Groups[1].Length);
+                blob = blob.Insert(m.Groups[1].Index, txtResearchPoints.Text);
+                break;
+            }
+
+            gdtDBHelper.setBlob(blob);
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (gdtDBHelper.isGameRunning())
+                if (MessageBox.Show("Game Dev Tycoon should NOT be running! Stop any instance of Game Dev Tycoon and click OK to continue, click Cancel to abort.", "Game Dev Tycoon is running!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    btnSave_Click(sender, e);
+                else
+                    return;
+            else
+                if (SaveBlob())
+                {
+                    gdtDBHelper.BackupDB();
+                    Update();
+                    MessageBox.Show("Stats updated successfully!", "Game stats updated!");
+                }
+        }
+
+        private void mainForm_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            AboutBox frm = new AboutBox();
+            frm.ShowDialog();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Update();
+        }
+
+        
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            if (gdtDBHelper.isGameRunning())
+            {
+                MessageBox.Show("Game Dev Tycoon should NOT be running! Stop any instance of Game Dev Tycoon and try again.", "Game Dev Tycoon is running!");
+                Application.Exit();
+            }
+            else
+            {
+                cmbSlot.SelectedIndex = 0;
+            }
+        }
+
+        private void cmbSlot_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gdtDBHelper.CurrentSlot = cmbSlot.SelectedItem.ToString();
+            Update();
+        }
+
+        private void btnRestoreDB_Click(object sender, EventArgs e)
+        {
+            gdtDBHelper.RestoreDB();
+        }
+
+    }
+}
---
+++ trunk/ game-dev-tycoon-trainer/gdtDBHelper/gdtDBHelper.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace GameDevTycoon
+{
+    public static class gdtDBHelper
+    {
+
+        public static string CurrentSlot = "slot_auto";
+        /// <summary>
+        /// Local database file default location.
+        /// </summary>
+        public static string Path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Game Dev Tycoon\Local Storage\file__0.localstorage";
+        /// <summary>
+        /// Used to convert the experience values from database to corresponding in game values.
+        /// For:
+        ///     Design
+        ///     Tecnology
+        ///     Speed
+        ///     Research
+        /// </summary>
+        /// <param name="factor"></param>
+        /// <returns></returns>
+        public static double FactorToInt(double factor)
+        {
+            return (factor / 2) * 1000;
+        }
+        /// <summary>
+        /// Used to convert the experience values from given int values to corresponding database double values.
+        /// For:
+        ///     Design
+        ///     Tecnology
+        ///     Speed
+        ///     Research
+        /// </summary>
+        /// <param name="factor"></param>
+        /// <returns></returns>
+        public static double FactorToDouble(double gameValue)
+        {
+            return (gameValue / 1000) * 2;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string[] GetSaveSlot(string path)
+        {
+            return new string[0];
+        }
+        /// <summary>
+        /// Creates a DB connection if none is existent
+        /// </summary>
+        private static void TryConnect()
+        {
+            if (Database.dbConnection == "" || Database.dbConnection == null)
+                Database.SetDatabase(gdtDBHelper.Path);
+        }
+        /// <summary>
+        /// String to byte[]
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+        /// <summary>
+        /// Byte[] to string
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        private static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+        /// <summary>
+        /// Checks if Game Dev Tycoon executable is listed in the running processes list
+        /// </summary>
+        /// <returns></returns>
+        public static bool isGameRunning()
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+                if (clsProcess.ProcessName == "GameDevTycoon")
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the blob file for the currentSlot.
+        /// </summary>
+        /// <returns></returns>
+        public static string getBlob()
+        {
+            TryConnect();
+
+            return GetString((byte[])Database.ExecuteScalar(String.Format("SELECT value from ItemTable WHERE key=\"{0}\"", gdtDBHelper.CurrentSlot)));
+        }
+        /// <summary>
+        /// Saves the blob file
+        /// </summary>
+        /// <param name="newBlob"></param>
+        /// <returns></returns>
+        public static int setBlob(string newBlob)
+        {
+            TryConnect();
+
+            string hex = BitConverter.ToString(GetBytes(newBlob));
+            hex = hex.Replace("-", "");
+            //UPDATE "main"."ItemTable" SET "value" = ?1 WHERE  "rowid" = 1689
+            // Create the SQL string.
+            string sql = String.Format("UPDATE ItemTable SET value=X'{0}' WHERE (key=\"{1}\")", hex, gdtDBHelper.CurrentSlot);
+            return Database.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// Backups file file__0.localstorage to file__0.localstorage.backup
+        /// backup file is overwritten
+        /// </summary>
+        public static void BackupDB()
+        {
+            if (isGameRunning())
+                return;
+            if (File.Exists(gdtDBHelper.Path.Replace("file__0.localstorage", "file__0.localstorage.backup")))
+                File.Delete(gdtDBHelper.Path.Replace("file__0.localstorage", "file__0.localstorage.backup"));
+            File.Copy(gdtDBHelper.Path, gdtDBHelper.Path.Replace("file__0.localstorage", "file__0.localstorage.backup"));
+        }
+
+        public static void RestoreDB()
+        {
+            if (isGameRunning())
+                return;
+            if (!File.Exists(gdtDBHelper.Path.Replace("file__0.localstorage", "file__0.localstorage.backup")))
+                return;
+            File.Delete(gdtDBHelper.Path);
+            File.Copy(gdtDBHelper.Path.Replace("file__0.localstorage", "file__0.localstorage.backup"), gdtDBHelper.Path);
+        }
+    }
+}
